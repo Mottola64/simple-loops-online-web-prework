ddfddd3838bf45b948d1e207475b30b03ac8e8d5
Michael Mottola <mottola64@gmail.com>
2019-02-18 21:34:55 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -17,7 +17,7 @@ end
 def while_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
 while number_of_times < 7
-number_of_times += 7
+number_of_times += 1
 puts phrase
 end
 
