85481245f364b8584ac0b5395d6729baa0cc5c01
Michael Mottola <mottola64@gmail.com>
2019-02-18 21:46:08 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -30,9 +30,8 @@ def until_iterator(number_of_times)
 end
 
 def for_iterator(number_of_times)
-  
-  
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+  for counter in 1..7 do
+    puts "Welcome to Flatiron School's Web Development Course!"
+  end
 end
 
